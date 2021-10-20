70254f79d2e3998bf8eef54b8457fd258ff7e8d3
hmorgado <91498952+hmorgado24@users.noreply.github.com>
2021-10-20 13:43:31 +0000

    Initial commit

---
+++ background.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+import json
+
+ledpin1 = 13
+ledpin2 = 19
+ledpin3 = 26
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledpin1, GPIO.OUT)
+GPIO.setup(ledpin2, GPIO.OUT)
+GPIO.setup(ledpin3, GPIO.OUT)
+
+pwm1 = GPIO.PWM(ledpin1, 100)
+pwm2 = GPIO.PWM(ledpin2, 100)
+pwm3 = GPIO.PWM(ledpin3, 100)
+pwm1.start(0)
+pwm2.start(0)
+pwm3.start(0)
+
+while True:
+  with open("cgicode.txt", 'r') as f:
+    data.json.load(f)
+    dutyCycle = str(data('slider1')) 
+    led = str(data('LED'))
+
+  if led = 1: 
+     pwm1.ChangeDutyCycle(dutyCycle)
+     time.sleep(.1)
+
+  if led = 2: 
+    pwm2.ChangeDutyCycle(dutyCycle)
+    time.sleep(.1)
+
+  if led = 3: 
+    pwm3.ChangeDutyCycle(dutyCycle)
+    time.sleep(.1)
---
+++ cgicode.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python37all
+import cgi
+import json
+import RPi.GPIO as GPIO
+
+import cgitb
+cgitb.enable()
+
+ledpin1 = 19
+ledpin2 =
+ledpin3 = 
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(ledpin1, GPIO.OUT)
+GPIO.setup(ledpin2, GPIO.OUT)
+GPIO.setup(ledpin3, GPIO.OUT)
+
+data = cgi.FieldStorage()
+L1 = data.getvalue('LED')
+s1 = data.getvalue('slider1')
+data = {"LED":L1, "slider1":s1}
+
+with open('cgicode.txt', 'w') as f:  
+  json.dump(data,f)
+
+print('Content-type: text/html\n\n')
+print('<html>')
+print('<meta http-equiv="refresh" content="30">')
+print('<body>') 
+print('<LED = " + data.getvalue("LED")><br>')
+print('<form action="/cgi-bin/cgicode.py" method="POST">')
+print('<input type="range" name="slider1" min="0" max="100" value="%s"><br>' % s1)
+print('<input type="submit" value="Change LED Brightness">')
+print('</form>')
+print(‘Brightness = %s' % s1)
+
+
+print('</body>')
+print('</html>')

ae3f73c121fc70ebb614b0e9d405607ef1be3ec0
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-10-20 17:06:52 +0000

    qwe

--- background.py
+++ background.py
@@ -25,7 +25,7 @@ pwm3.start(0)
 while True:
   with open("cgicode.txt", 'r') as f:
     data = json.load(f)
-    pwm1.ChangeDutyCycle(float(data['LED1']))
+    pwm1.ChangeDutyCycle(float(data["LED1"]))
     pwm2.ChangeDutyCycle(float(data['LED2']))
     pwm3.ChangeDutyCycle(float(data['LED3']))
     time.sleep(.1)
